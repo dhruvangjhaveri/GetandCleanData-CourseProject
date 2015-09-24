e5b173adb41a9961f68783624a8bf7d7061ecde9
Dhruvang <dhruvang.jhaveri@gmail.com>
2015-09-24 23:05:26 +0530

    First Commit

---
+++ run_analysis.R
@@ -0,0 +1,29 @@
+## Installing Required Packages
+if(!("data.table" %in% installed.packages())) install.packages("data.table")
+if(!("dplyr" %in% installed.packages())) install.packages("dplyr")
+library(data.table)
+library(dplyr)
+
+## Reading Data from Files
+data <- fread("./UCI HAR Dataset/test/subject_test.txt")
+data <- rbind(data, fread("./UCI HAR Dataset/train/subject_train.txt"))
+x <- fread("./UCI HAR Dataset/test/y_test.txt")
+x <- rbind(x, fread("./UCI HAR Dataset/train/y_train.txt"))
+x <- as.factor(x[,V1])
+levels(x) <- as.character(fread("./UCI HAR Dataset/activity_labels.txt")[,V2]) 
+data[,x:=x]
+y <- fread("./UCI HAR Dataset/test/X_test.txt")
+y <- rbind(y, fread("./UCI HAR Dataset/train/X_train.txt"))
+data <- cbind(data, y)
+rm(x,y)
+
+## Naming Variables of Data
+names(data) <- c("Subject", "Activity", fread("./UCI HAR Dataset/features.txt")[,V2])
+
+## Substetting & Arranging Data
+data <- select(data, c(1:2, grep("mean()",names(data), fixed = T), grep("std()",names(data), fixed = T)))
+data <- arrange(data, Subject, Activity)
+
+## Summarising Data
+data2 <- group_by(data, Subject, Activity)
+data2 <- summarise_each(data2, funs(mean))
